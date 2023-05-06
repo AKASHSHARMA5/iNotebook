2abee563944fee47430d1cbd0e405a0a4ae47618
AKASHSHARMA5 <131224911+AKASHSHARMA5@users.noreply.github.com>
2023-05-06 12:42:23 +0530

    iNotebook

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const mongoURI = "mongodb://127.0.0.1:27017/"
+
+const connectToMongo = async () => {
+    try {
+        mongoose.set("strictQuery", false)
+        mongoose.connect(mongoURI)
+        console.log("connected to Mongo successfully")
+    }
+    catch (error) {
+        console.log(error)
+        process.exit()
+    }
+
+}
+module.exports = connectToMongo;
